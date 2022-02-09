9db9d1d598bf362fd4be72f9b89e8882bd8ca5e8
Yudi Tan <yudi98@berkeley.edu>
2022-02-08 21:43:47 -0800

    Updated user card's profile pic to be rounded

--- frontend/components/user/user-card.tsx
+++ frontend/components/user/user-card.tsx
@@ -1,4 +1,5 @@
 import { styled } from "@mui/material/styles";
+import Avatar from "@mui/material/Avatar";
 import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
@@ -8,21 +9,25 @@ import Container from "@mui/material/Container";
 const UserCard = () => {
   return (
     <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component="img"
-        height="380"
-        image="https://i.mydramalist.com/vK4lp_5c.jpg"
-      />
-      <CardContent>
-        <Container>
-          <Typography gutterBottom variant="h5" component="div">
-            @IU
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Hi i'm IU.
-          </Typography>
-        </Container>
-      </CardContent>
+      <Container>
+        <Avatar
+          alt="profile picture"
+          src="https://i.mydramalist.com/vK4lp_5c.jpg"
+          sx={{ width: 200, height: 200 }}
+        />
+      </Container>
+      <Container>
+        <CardContent>
+          <Container>
+            <Typography gutterBottom variant="h5" component="div">
+              @IU
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Hi i'm IU.
+            </Typography>
+          </Container>
+        </CardContent>
+      </Container>
     </Card>
   );
 };
