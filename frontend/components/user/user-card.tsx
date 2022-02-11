dcf1fd1833f4b6e9bfd3837ba8cd70cad8603a13
Yudi Tan <yudi98@berkeley.edu>
2022-02-10 20:23:07 -0800

    Refined user card styling to match wireframe

--- frontend/components/user/user-card.tsx
+++ frontend/components/user/user-card.tsx
@@ -3,31 +3,35 @@ import Avatar from "@mui/material/Avatar";
 import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
 
 const UserCard = () => {
   return (
     <Card sx={{ maxWidth: 345 }}>
-      <Container>
-        <Avatar
-          alt="profile picture"
-          src="https://i.mydramalist.com/vK4lp_5c.jpg"
-          sx={{ width: 200, height: 200 }}
-        />
-      </Container>
-      <Container>
+      <Stack direction="column" alignItems="center" justifyContent="center">
+        <Container>
+          <Avatar
+            alt="profile picture"
+            src="https://i.mydramalist.com/vK4lp_5c.jpg"
+            sx={{
+              width: "100%",
+              height: "auto",
+              maxWidth: 180,
+              maxHeight: 180,
+            }}
+          />
+        </Container>
         <CardContent>
-          <Container>
-            <Typography gutterBottom variant="h5" component="div">
-              @IU
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Hi i'm IU.
-            </Typography>
-          </Container>
+          <Typography gutterBottom variant="h5" component="div">
+            @IU
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Hi i'm IU.
+          </Typography>
         </CardContent>
-      </Container>
+      </Stack>
     </Card>
   );
 };
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,18 +1,19 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import type { NextPage } from "next";
+import Head from "next/head";
 
 const Home: NextPage = () => {
   return (
     <div>
       <Head>
         <title>Keebs</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
 
       <main>
-          <h1>Landing Page</h1>
+        <h1>Landing Page</h1>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
